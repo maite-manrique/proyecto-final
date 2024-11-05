ae4ab6ec65cf6bbb4291b34cb9cad816cce41abd
maite-manrique <maitemanrique2019@gmail.com>
2024-11-04 22:59:18 -0300

    Nombre de la tienda

--- config/web.php
+++ config/web.php
@@ -10,6 +10,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'name' => ' Vina Blossom', //Nombre de nuestra tienda
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
