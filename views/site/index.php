0171c8f1291e2402404f748d656d949fbeffe50d
maite-manrique <maitemanrique2019@gmail.com>
2024-11-15 17:12:41 -0300

    index Maia

--- views/site/index.php
+++ views/site/index.php
@@ -2,20 +2,9 @@
 
 /** @var yii\web\View $this */
 
+use yii\bootstrap5\Carousel;
+
 $this->title = 'Proyecto Final WAWA';
-?>
-<?php
-// session_start();
-// include 'conexion.php';
-
-// $categoria = isset($_GET['categoria']) ? $_GET['categoria'] : '';
-// $sql = "SELECT * FROM productos";
-// if ($categoria) {
-//     $sql .= " WHERE categoria = '$categoria'";
-// }
-
-// $result = $conn->query($sql);
-// $this->title="Tienda en linea";
 ?>
 <div class="site-index">
     <br><br>
@@ -26,96 +15,78 @@ $this->title = 'Proyecto Final WAWA';
         </form>
     </div>
     <div class="body-content">
+        <div class="productos">
+            <?php
+            // // if ($result->num_rows > 0) {
+            //     while ($row = $result->fetch_assoc()) {
+            //         echo "<div class='producto'>";
+            //         echo "<img src='imagenes/" . $row['imagen'] . "' alt='" . $row['nombre'] . "'>";
+            //         echo "<h2>" . $row['nombre'] . "</h2>";
+            //         echo "<p>" . $row['descripcion'] . "</p>";
+            //         echo "<p><strong>Precio: $" . $row['precio'] . "</strong></p>";
+            //         echo "<form method='POST' action='carrito.php'>";
+            //         echo "<input type='hidden' name='producto_id' value='" . $row['id'] . "'>";
+            //         echo "<button type='submit' name='agregar_carrito'>Añadir al carrito</button>";
+            //         echo "</form>";
+            //         echo "</div>";
+            //     }
+            // } else {
+            //     echo "<p>No se encontraron productos</p>";
+            // }
+            ?>
+        </div>
 
-    <div class="productos">
-        <?php
-        // // if ($result->num_rows > 0) {
-        //     while ($row = $result->fetch_assoc()) {
-        //         echo "<div class='producto'>";
-        //         echo "<img src='imagenes/" . $row['imagen'] . "' alt='" . $row['nombre'] . "'>";
-        //         echo "<h2>" . $row['nombre'] . "</h2>";
-        //         echo "<p>" . $row['descripcion'] . "</p>";
-        //         echo "<p><strong>Precio: $" . $row['precio'] . "</strong></p>";
-        //         echo "<form method='POST' action='carrito.php'>";
-        //         echo "<input type='hidden' name='producto_id' value='" . $row['id'] . "'>";
-        //         echo "<button type='submit' name='agregar_carrito'>Añadir al carrito</button>";
-        //         echo "</form>";
-        //         echo "</div>";
-        //     }
-        // } else {
-        //     echo "<p>No se encontraron productos</p>";
-        // }
-        ?>
-    </div>
-    <div class="row">
-        <section class="Productos-destacados">
-            <h2>Productos destacados</h2>
-            
-            <div class="productos">
-                <!--Ejemplo de los productos destacados -->
-                <div class="producto col-lg-4 col-sm-6">
-                    <img src="multimedia/" alt="Labial">
-                    <p>Labial Mate</p>
-                    <p>$2500</p>
+            <div class="row">
+        <!-- Tarjeta 1 -->
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card">
+                <img src="@web/image/LabialMate.jpg" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Producto 1</h5>
+                    <p class="card-text">Descripción breve del producto 1. Destaca sus principales características.</p>
+                    <a href="#" class="btn btn-primary">Ver más</a>
                 </div>
-
-<<<<<<< Updated upstream
-                <div class="producto col-lg-4 col-sm-6">
-                    <img src="IMAGENES/Balsamo Labial" alt="Delineador Liquido">
-                    <p>Delineador Liquido</p>
-                    <p>$3000</p>
-                </div>
-
-                <div class="producto col-lg-4 col-sm-6">
-                    <img src="multimedia/" alt="Sombra de ojos">
-                    <p>Sombra de ojos</p>
-                    <p>$5000</p>
-                </div>
-                
             </div>
-            <!-- Podemos repetir este div de productos para los que destaquemos -->
-        </section>
+        </div>
+
+        <!-- Tarjeta 2 -->
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card">
+                <img src="@web/image/Sombradeojos.jpg" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Producto 2</h5>
+                    <p class="card-text">Descripción breve del producto 2. Información adicional relevante.</p>
+                    <a href="#" class="btn btn-primary">Ver más</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Tarjeta 3 -->
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card">
+                <img src="@web/image/DelineadorLiquido.jpg" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Producto 3</h5>
+                    <p class="card-text">Descripción breve del producto 3. Aspectos a destacar.</p>
+                    <a href="#" class="btn btn-primary">Ver más</a>
+                </div>
+            </div>
+        </div>
     </div>
-         <button>Agregar al carrito <i class="fab fa-shopify"></i></button>
+
+        <button>Agregar al carrito <i class="fab fa-shopify"></i></button>
         <section class="Suscripción">
             <p>Suscribete para recibir ofertas exclusivas</p>
             <input type="email" placeholder="Correo Electrónico">
             <button>Suscribirse</button>
         </section>
-=======
-    <section class="Productos-destacados">
-        <h2>Productos destacados</h2>
-        <div class="productos">
-            <!--Ejemplo de los productos destacados -->
-            <div class="producto">
-                <img src="Labial_Mate.jpg" alt="Labial">
-                <p>Labial Mate</p>
-                <p>$2000</p>
+        <footer>
+            <p>© 2024 Tienda online de maquillaje. Todos los derechos reservados</p>
+            <div class="Encuentranos en nuestras redes sociales">
+                <a href="">Instagram</a>
+                <a href="">Facebook</a>
+                <a href="">TikTok</a>
             </div>
-            <button>Agregar al carrito</button>
-        </div>
-    <!-- Podemos repetir este div de productos para los que destaquemos -->
-    </section>
-
-    <section class="suscripción">
-        <p>Suscribete para recibir ofertas exclusivas</p>
-        <input type="email" placeholder="Correo Electrónico">
-        <button>Suscribirse</button>
-    </section>
-
-    <footer>
-        <p>© 2024 Tienda online de maquillaje. Todos los derechos reservados</p>
-        <div class="Encuentranos en nuestras redes sociales">
-            <a href="">Instagram</a>
-            <a href="">Facebook</a>
-            <a href="">TikTok</a>
-        </div>
-    </footer>
-</html>
-
-<?php
-// $conn->close();
-?>
->>>>>>> Stashed changes
+        </footer>
+    </div>
 </div>
-
